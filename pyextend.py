048951f448264a85a24ddce921a3c4e29b68bcc4
SamP-S <sam.ps1@outlook.com>
2024-02-15 15:55:00 +0000

    Add timing info

--- pyextend.py
+++ pyextend.py
@@ -1,6 +1,7 @@
 import configparser
 import os
 import sys
+import time
 
 # import ocve
 OCVE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "ocve"))
@@ -12,6 +13,9 @@ CONFIG = configparser.ConfigParser()
 PROFILE = CONFIG["DEFAULT"]
 PWD = os.getcwd()
 
+DIR_COUNT = 0
+FILE_COUNT = 0
+
 def apply_transform(in_path, out_path):
     img = ocve.read_img(in_path)
     img = ocve.gaussian_noise(
@@ -20,18 +24,19 @@ def apply_transform(in_path, out_path):
         mean=0,
         variance=400)
     ocve.save_img(out_path, img)
-    # print("IN:", in_path)
-    # print("OUT:", out_path)
             
 def dir_search(in_dir, out_dir):
+    global DIR_COUNT, FILE_COUNT
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
     for (root, dirs, files) in os.walk(in_dir):
         for dir in dirs:
+            DIR_COUNT += 1
             class_dir = os.path.join(out_dir, dir)
             if not os.path.exists(class_dir):
                 os.mkdir(class_dir)
         for file in files:
+            FILE_COUNT += 1
             rel_path = os.path.join(os.path.basename(root), file)
             in_path = os.path.join(root, file)
             out_path = os.path.join(out_dir, rel_path)
@@ -59,11 +64,18 @@ def save_cfg():
         CONFIG.write(configfile)
 
 def main():
+    start_app = time.time()
+
     save_cfg()
     load_cfg()
     
     dir_search(PROFILE["brick_data_dir"], PROFILE["output_data_dir"])
     # save_cfg()
+    end_app = time.time()
+    dt = end_app-start_app
+    print(f"Program Duration = {dt:.2f}s @ {dt/FILE_COUNT:.2f}")
+    print(f"Data = {FILE_COUNT} files in {DIR_COUNT} dirs")
+
 
 if __name__ == "__main__":
     main()
